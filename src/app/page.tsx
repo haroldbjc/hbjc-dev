7a5eae07372ab10adac7b0fe25090d718279bb3a
Harold <harold.bjc@gmail.com>
2025-02-05 16:12:42 +0800

    chore: update README

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import { ModeToggle } from "@/components/theme-toggle";
-import { Button } from "@/components/ui/button";
+import { FlowerIcon } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen p-8 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Button>Click</Button>
-          <ModeToggle />
-        </div>
+      <main className="flex flex-col gap-8 items-center">
+        <FlowerIcon />
+        <p>
+          如此绚丽的花朵，不该在绽放之前就枯萎。我会赠予你璀聚的祝福，而你的灵魂，也将会绽放更耀眼的光辉。
+        </p>
+        <p>~ 爱莉希雅</p>
+        <ModeToggle />
       </main>
     </div>
   );
